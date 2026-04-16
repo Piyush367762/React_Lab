185b18d4581c0a16a40283d0be69e69357f8fe36
Piyush <piyush9c24@gmail.com>
2026-04-16 11:54:50 +0530

    proper features added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import {Link} from "react-router-dom";
+import "./Navbar.css";
+import logo from "../assets/logo.png";
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="logo" style={{ height: "40px" }}/>
+      </div>
 
-function Navbar(){
-    return (
-    <nav style={{ display: "flex", gap: "20px", padding: "10px" }}>
+      <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/practice">Practice</Link>
-        <Link to="/docs">Documentation</Link>
-        <Link to="/about">About Us</Link>
+        <Link to="/docs">Docs</Link>
+        <Link to="/about">About</Link>
+      </div>
     </nav>
-    );
+  );
 }
 export default Navbar;
