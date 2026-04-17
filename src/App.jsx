6e2a10ca775e8f445d0728a49ce6b5116c7af9cd
Piyush <piyush9c24@gmail.com>
2026-04-17 09:09:50 +0530

    analysis

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Practice from "./pages/Pratice.jsx";
 import Docs from "./pages/Docs";
 import Home from "./pages/Home";
+
 //import About from "./pages/About";
 
 function App() {
